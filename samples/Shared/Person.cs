959a724be17ca75f35ba91fe3e7ccdb4c1923e65
Prathamesh Shende <63373198+imprathamesh@users.noreply.github.com>
2025-06-12 14:56:01 +0530

    Add project files.

---
+++ samples/Shared/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sample.Shared
+{
+    public class Person
+    {
+        public Person() { }
+        public Person(int id, string firstname, string lastname, int age, string location)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+            Location = location;
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string FullName { get => Firstname + " " + Lastname; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+    }
+}
